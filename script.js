03f46661140b755861ff01cd02de757c4c935b88
raniabejja-data <raniabj35@gmail.com>
2026-02-20 09:10:04 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// Animation au scroll
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.scroll-reveal');
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+
+        if (revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('active');
+        }
+    }
+}
+
+function toggleMenu() {
+    alert("Le menu 'Votre Essence Royale' arrive bientôt !");
+}
